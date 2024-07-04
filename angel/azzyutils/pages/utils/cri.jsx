eba1b7d698f65a3027a9902a9a9618b5195a49e3
Azrae <whislist1@gmail.com>
2024-07-04 15:52:22 +0800

    update Cri.jsx

--- angel/azzyutils/pages/utils/cri.jsx
+++ angel/azzyutils/pages/utils/cri.jsx
@@ -1,8 +1,9 @@
 import { storage } from "@vendetta/plugin"
 import { numToHex, openSheet, UIElements } from "../../../../lib/utility"
-import { ReactNative } from "@vendetta/metro/common"
+import { clipboard, ReactNative } from "@vendetta/metro/common"
 import { findByName } from "@vendetta/metro";
 import { getAssetIDByName } from "@vendetta/ui/assets";
+import { showToast } from "@vendetta/ui/toasts";
 
 const defaultImageURL = 'https://cdn.discordapp.com/role-icons/1222207470714486825/bc3ef24c3f220155f90e55cc0cb0d0cf.png?size=512'
 const { FormRow, FormSwitch, TouchableOpacity, Image, FormDivider, FormIcon, FormInput } = UIElements
@@ -36,18 +37,25 @@ export default function CustomRoleIconPage() {
 
 	const SUCRI = storage?.utils?.customRoleIcon;
 	
+	const copyDefaultURL = () => {
+		clipboard.setString("https://cdn.discordapp.com/role-icons/1222207470714486825/bc3ef24c3f220155f90e55cc0cb0d0cf.png?size=512")
+		showToast("Copied placeholder URL", getAssetIDByName("toast_copy_link"))
+	}
+
 	return (<>
 		<FormRow
-			label="Icon Preview"
+			label="Icon Preview"		
 			trailing={
-				<Image
-					source={{ uri: SUCRI?.source || defaultImageURL }}
-					style={{ 
-						width: 128, 
-						height: 128,
-						borderRadius: 10, 
-					}}
-				/>
+				<TouchableOpacity onPress={copyDefaultURL}>
+					<Image
+						source={{ uri: SUCRI?.source || defaultImageURL }}
+						style={{ 
+							width: 128, 
+							height: 128,
+							borderRadius: 10, 
+						}}
+					/>
+				</TouchableOpacity>
 			}
 		/>
 		<FormDivider />
