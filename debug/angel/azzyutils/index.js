9ea6b2f5fff4ff392e15810ef5c66754e0120597
Angelix1 <Angelix1@users.noreply.github.com>
2024-07-04 07:44:48 +0000

    deploy: 1d11709b45952da1845e791d3431372aba864903

--- debug/angel/azzyutils/index.js
+++ debug/angel/azzyutils/index.js
@@ -481,7 +481,7 @@ function CustomRoleIconPage() {
     label: "Icon Preview",
     trailing: /* @__PURE__ */ React.createElement(Image$1, {
       source: {
-        uri: defaultImageURL
+        uri: SUCRI?.source || defaultImageURL
       },
       style: {
         width: 128,
@@ -911,14 +911,22 @@ function patchCustomUsernameColor(row) {
 function patchCustomRoleIcon(row) {
   if (row?.message) {
     if (UserStore$1?.getCurrentUser?.()?.id == row?.message?.authorId) {
-      let CRI = plugin.storage?.utils?.customRoleIcon || {
-        name: "BlobCatSip",
-        source: "https://cdn.discordapp.com/role-icons/1222207470714486825/bc3ef24c3f220155f90e55cc0cb0d0cf.png?size=512",
-        alt: "Role icon, BlobCatSip",
-        size: 18
-      };
-      CRI.alt ?? (CRI.alt = `Role icon, ${CRI.name}`);
-      row.message.roleIcon = CRI;
+      const CRI = plugin.storage?.utils?.customRoleIcon;
+      let roleIconObject = {};
+      if (!CRI?.name && !CRI?.source && !CRI?.size) {
+        roleIconObject = {
+          name: "BlobCatSip",
+          source: "https://cdn.discordapp.com/role-icons/1222207470714486825/bc3ef24c3f220155f90e55cc0cb0d0cf.png?size=512",
+          alt: "Role icon, BlobCatSip",
+          size: 18
+        };
+      } else {
+        roleIconObject.name = CRI.name;
+        roleIconObject.source = CRI.source;
+        roleIconObject.size = CRI.size;
+        roleIconObject.alt = `Role icon, ${CRI.name}`;
+      }
+      row.message.roleIcon = roleIconObject;
     }
     return row;
   }
